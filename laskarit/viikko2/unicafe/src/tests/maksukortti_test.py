4260350799e26ae7d5e04229c182c89c36e7d981
Anna Kangasmaa <anna.kangasmaa@helsinki.fi>
2021-03-26 18:32:40 +0200

    korjattiin testimetodien nimiä

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -8,25 +8,25 @@ class TestMaksukortti(unittest.TestCase):
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
 
-    def saldo_alussa_oikein(self):
+    def test_saldo_alussa_oikein(self):
         self.assertEqual(str(self.maksukortti), "saldo: 10")
     
-    def rahan_lataaminen_toimii_oikein(self):
+    def test_rahan_lataaminen_toimii_oikein(self):
         self.maksukortti.lataa_rahaa(10)
         self.assertEqual(str(self.maksukortti), "saldo: 20")
     
-    def rahaa_vahenee_oikea_maara(self):
+    def test_rahaa_vahenee_oikea_maara(self):
         self.maksukortti.ota_rahaa(5)
         self.assertEqual(str(self.maksukortti), "saldo: 5")    
 
-    def saldo_ei_muutu_jos_raha_ei_riita(self):
+    def test_saldo_ei_muutu_jos_raha_ei_riita(self):
         self.maksukortti.ota_rahaa(15)
         self.assertEqual(str(self.maksukortti), "saldo: 10")
 
-    def ota_rahaa_palauttaa_true_kun_raha_riittaa(self):
+    def test_ota_rahaa_palauttaa_true_kun_raha_riittaa(self):
         self.maksukortti.ota_rahaa(5)
         self.assertTrue(self.maksukortti.ota_rahaa)
     
-    def ota_rahaa_palauttaa_false_kun_raha_ei_riita(self):
+    def test_ota_rahaa_palauttaa_false_kun_raha_ei_riita(self):
         self.maksukortti.ota_rahaa(15)
         self.assertFalse(self.maksukortti.ota_rahaa)
