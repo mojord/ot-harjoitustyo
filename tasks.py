72d4e729676074ebb53fcbc67398d3df239cdaec
Anna Kangasmaa <anna.kangasmaa@helsinki.fi>
2021-04-13 18:43:48 +0300

    lisättiin taskeja

--- tasks.py
+++ tasks.py
@@ -2,5 +2,13 @@ from invoke import task
 
 @task
 def start(ctx):
-    ctx.run("python3 src/textinterface.py)
+    ctx.run("python3 src/textinterface.py")
 
+def test(ctx):
+    ctx.run("pytest.src")
+
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest src")
+
+def coverage_report(ctx):
+    ctx.run("coverage html")
