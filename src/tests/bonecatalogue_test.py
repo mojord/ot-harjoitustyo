5f1be7762c8ad4213f57212c8d3239bbf3413c22
Anna Kangasmaa <anna.kangasmaa@helsinki.fi>
2021-04-14 13:32:44 +0300

    testitiedostokokeiluja

--- src/tests/bonecatalogue_test.py
+++ src/tests/bonecatalogue_test.py
@@ -7,10 +7,11 @@ TESTDATA = os.path.join(os.path.dirname(__file__), "testaus.csv")
 class TestBonecatalogue(unittest.TestCase):
     def setUp(self):
         self.catalogue = Bonecatalogue()
-        self.testdata = self.catalogue.read_file(TESTDATA)
+#        self.testdata = self.catalogue.read_file(TESTDATA)
 	
 
     def test_give_species_toimii_oikein(self):
+        self.catalogue.read_file(TESTDATA)
         species = "Bos taurus"
         tulos = self.catalogue.give_species(species)
         self.assertEqual(tulos, "Bos taurus specimens: 1, identified bones: [('femur', '1')]")
